c6ec42a03485a57f01b63f3075656635814e8505
Kakalasek <honziccekkj@seznam.cz>
2024-09-30 20:14:25 +0200

    another one

---
+++ 3/Vodni_trasa/main.py
@@ -0,0 +1,39 @@
+rivers = [
+    {
+        "name": "labe",
+        "spring": "krkonoše",
+        "confluence_place": "",
+        "confluence_river": ""
+    },
+    {
+        "name": "jizera",
+        "spring": "jizerské hory",
+        "confluence_place": "lázně toušeň",
+        "confluence_river": "labe"
+    },
+    {
+        "name": "vltava",
+        "spring": "šumava",
+        "confluence_place": "mělník",
+        "confluence_river": "labe"
+    },
+    {
+        "name": "berounka",
+        "spring": "",
+        "confluence_place": "praha-zbraslav",
+        "confluence_river": "vltava"
+    },
+    {
+        "name": "ohře",
+        "spring": "smrčiny",
+        "confluence_place": "litoměřice",
+        "confluence_river": "labe"
+    },
+    {
+        "name": "teplá",
+        "spring": "zádub-zavišín",
+        "confluence_place": "karlovy vary",
+        "confluence_river": "ohře"
+    }
+]
+
