b4d3aa8ceb88981a9ab7b7e04f177e83a0853473
kakalasek <honzicekkj@seznam.cz>
2024-10-03 20:21:16 +0200

    trasa

--- 3/Vodni_trasa/main.py
+++ 3/Vodni_trasa/main.py
@@ -1,39 +1,82 @@
-rivers = [
-    {
-        "name": "labe",
-        "spring": "krkonoše",
-        "confluence_place": "",
-        "confluence_river": ""
-    },
-    {
-        "name": "jizera",
-        "spring": "jizerské hory",
-        "confluence_place": "lázně toušeň",
-        "confluence_river": "labe"
-    },
-    {
-        "name": "vltava",
-        "spring": "šumava",
-        "confluence_place": "mělník",
-        "confluence_river": "labe"
-    },
-    {
-        "name": "berounka",
-        "spring": "",
-        "confluence_place": "praha-zbraslav",
-        "confluence_river": "vltava"
-    },
-    {
-        "name": "ohře",
-        "spring": "smrčiny",
-        "confluence_place": "litoměřice",
-        "confluence_river": "labe"
-    },
-    {
-        "name": "teplá",
-        "spring": "zádub-zavišín",
-        "confluence_place": "karlovy vary",
-        "confluence_river": "ohře"
-    }
-]
+graph = {
+    0: [1, 2],
+    1: [0],
+    2: [0, 3],
+    3: [2, 4, 7],
+    4: [3, 6, 5],
+    5: [4],
+    6: [4],
+    7: [3],
+}
 
+mapping = {
+    0:  {"name": "litomerice", "confluence": {"in": "ohre", "out": "labe"}},
+    1:  {"name": "smrciny", "river": "ohre"},
+    2:  {"name": "melnik", "confluence": {"in": "vltava", "out": "labe"}},
+    3:  {"name": "lazne tousen", "confluence": {"in": "jizera", "out": "labe"}},
+    4:  {"name": "hradec kralove", "confluence": {"in": "orlice", "out": "labe"}},
+    5:  {"name": "orlicke hory", "river": "orlice"},
+    6:  {"name": "krkonose", "river": "labe"},
+    7:  {"name": "jizerske hory", "river": "jizera"}
+}
+
+
+def perform_mapping(map: dict, path: list) -> list:
+    mapped_path = []
+
+    for point in path:
+        mapped_path.append(map[point])
+
+    return mapped_path
+
+def reverse_mapping(map: dict, value: str) -> int:
+    for k,v in map.items():
+        if v['name'] == value:
+            return k
+
+def inorder_traversal(v, d, traversed: list, path: list):
+    if v in traversed or d in traversed:
+        return
+
+    path.append(v)
+    traversed.append(v)
+
+    for point in graph[v]:
+        inorder_traversal(point, d, traversed, path)
+
+    if d not in traversed:
+        path.pop()
+
+point_a = input("Zvolte místo A: ")
+point_b = input("Zvolte místo B: ")
+point_a = point_a.lower().strip()
+point_b = point_b.lower().strip()
+
+point_a = reverse_mapping(mapping, point_a)
+point_b = reverse_mapping(mapping, point_b)
+
+path = []
+
+inorder_traversal(point_a, point_b, [], path) 
+
+path = perform_mapping(mapping, path)
+
+
+print(f"Z {path[0]['name']}")
+print(f"Po ", end='')
+if 'river' in path[0].keys():
+    print(path[0]['river'])
+else:
+    print(path[0]['confluence']['out'])
+
+print()
+
+for point in path[1:]:
+    print(f"Do {point['name']}")
+
+    if 'river' in point.keys():
+        print(f"Po {point['river']}")
+    else:
+        print(f"Po {point['confluence']['out']}")
+
+    print()
