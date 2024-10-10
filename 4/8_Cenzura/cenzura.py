5e1520fc7d14460aaa6d2f2e3d4dba04feca9735
kakalasek <honzicekkj@seznam.cz>
2024-10-10 22:35:32 +0200

    cenzura

--- 4/8_Cenzura/cenzura.py
+++ 4/8_Cenzura/cenzura.py
@@ -8,22 +8,25 @@ def create_sql(author: str, text: str) -> str:
         'Jára Cimrman'
     ]
 
-    censored_names_regex = f""
+    censored_names_regex = f"((Ondra|Ondřej|)[ ]*Mandík|(Alena|)[ ]*Reichlová|(Jaroslav|Jára|)[ ]*Cimrman)"
 
-    if not re.match(r"^[a-zA-Z]{1}[a-zA-Z'- ]*[a-zA-z]$", author):
+    if not re.match(r"^[a-zA-Z]{1}[a-zA-Z' -]*[a-zA-z]$", author):
         raise ValueError("Invalid author")
 
 
 
     for name in list_of_censored_names:
         while True:
-
             appearence = re.search(censored_names_regex, text)
+            if appearence:
+                text = text.replace(appearence.group().strip(), "Automatically censored", 1)
+                continue
+            break
 
     return f"INSERT INTO PRISPEVEK(AUTHOR, TEXT) VALUES ('{author}', '{text}')"
 
 try:
-    statement = create_sql("O'Connor", "Alena Reichlová a Mandík")
+    statement = create_sql("O'Connor", "Alena Reichlová a Mandík je dvojice plna .. no nic")
     print(statement)
 except ValueError as e:
     print(e)
