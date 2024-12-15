78e9ee715ea9296a1ad03d63bfefdef334a9a947
Kakalasek <example@email.com>
2024-12-15 18:19:05 +0100

    binary fecking shit

--- 13_Prace_se_soubory/4_Binarni_soubor_jako_zamek/binarni_soubor_jako_zamek.py
+++ 13_Prace_se_soubory/4_Binarni_soubor_jako_zamek/binarni_soubor_jako_zamek.py
@@ -1,8 +1,28 @@
-with open('lock.dat', 'b') as file:
-    lock = file.read(1)
-    if lock is None:
-        file.seek(0)
-        file.write(b'\x00')
-    
-    if file.read(0) == b'\x00':
-        pass
+import os
+from pathlib import Path
+
+def check_existence(filepath: str):
+    file = Path(filepath)
+    if not file.is_file():
+        with open(filepath, "wb") as binaryFile:
+            binaryFile.write(b'\x00')
+
+def write_lock(filepath: str, data: str):
+    with open(filepath, 'rb+') as lock:
+        sign = lock.read(1)
+        if sign == b'\x00':
+            lock.seek(0)
+            lock.write(b'\xFF')
+        else:
+            raise PermissionError
+        
+        lock.write(data.encode())
+        lock.seek(0)
+        lock.write(b'\x00')
+
+if __name__ == "__main__":
+    try:
+        check_existence("lock.dat")
+        write_lock("lock.dat", "Hello there")
+    except PermissionError:
+        print("There is already another running instance of this program")
