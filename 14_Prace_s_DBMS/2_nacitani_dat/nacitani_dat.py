c1d0328259b496a67857979b0f6e9360146d13ab
Kakalasek <example@email.com>
2025-01-06 18:51:08 +0100

    select from database

--- 14_Prace_s_DBMS/2_nacitani_dat/nacitani_dat.py
+++ 14_Prace_s_DBMS/2_nacitani_dat/nacitani_dat.py
@@ -3,7 +3,24 @@ import mysql.connector
 connection = mysql.connector.connect(
     host="127.0.0.1",
     user="app1user",
-    password="student"
+    password="student",
+    database="app1"
 )
+
+sqlSelectAll = "SELECT * FROM Death;"
+sqlSelectCount = "SELECT COUNT(*) FROM Death;"
+
+cursor = connection.cursor()
+cursor.execute(sqlSelectAll)
+allRecords = cursor.fetchall()
+
+for row in allRecords:
+    print(row[0], row[1], row[2], sep=', ')
+
+cursor.execute(sqlSelectCount)
+recordCount = cursor.fetchall()
+
+for row in recordCount:
+    print(row[0])
 
 connection.close()
