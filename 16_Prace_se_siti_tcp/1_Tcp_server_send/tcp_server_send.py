970e75f7700759b25ccded4c39d2213602c419d3
Kakalasek <example@email.com>
2025-01-20 11:54:37 +0100

    tcp send

---
+++ 16_Prace_se_siti_tcp/1_Tcp_server_send/tcp_server_send.py
@@ -0,0 +1,29 @@
+import socket
+
+host = "127.0.0.1"
+port = 65533
+
+server_inet_address = (host, port)
+
+server_socket = socket.socket()
+server_socket.bind(server_inet_address)
+server_socket.listen()
+print("Server start on " + str(server_inet_address[0]) + ":" + str(server_inet_address[1]))
+
+connection, client_inet_address = server_socket.accept()
+print("Client connection accepted from " + str(client_inet_address[0]) + ":" + str(client_inet_address[1]))
+
+message = "Hello there\n"
+message = bytes(message, "utf-8")
+print("Sending bytes:")
+
+for byte in message:
+    print(bin(byte))
+
+connection.send(message)
+
+connection.close()
+print("Client connection closed")
+
+server_socket.close()
+print("Server is closed")
