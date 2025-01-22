fa24262ce946fb59c4421a9a2ee0b2ee0eed671b
Kakalasek <example@email.com>
2025-01-22 17:26:23 +0100

    basic recv structure

--- 16_Prace_se_siti_tcp/2_Tcp_server_recv/tcp_server_recv.py
+++ 16_Prace_se_siti_tcp/2_Tcp_server_recv/tcp_server_recv.py
@@ -1,1 +1,32 @@
 import socket
+
+try:
+
+    host = "127.0.0.1"
+    port = 65532
+
+    server_inet_address = (host, port)
+
+    server_socket = socket.socket()
+    server_socket.bind(server_inet_address)
+    server_socket.listen()
+    while True:
+        try:
+            connection, client_inet_address = server_socket.accept()
+            connection.send(b'Hello there\n')
+            while True:
+                something = connection.recv(20)
+                if something.strip() == b'end':
+                    connection.send(b'Quitting\n')
+                    break
+                connection.send(something)
+        except Exception as e:
+            print(e)
+        finally:
+            connection.close()
+
+except Exception as e:
+    print(e)
+finally:
+    server_socket.close()
+    print("Server is closed")
