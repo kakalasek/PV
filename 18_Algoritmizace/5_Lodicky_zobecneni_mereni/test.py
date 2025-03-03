e9288b1957315efc15ce0d25ad46b7a55d7db17e
Kakalasek <example@email.com>
2025-03-03 18:23:53 +0100

    s

--- 18_Algoritmizace/5_Lodicky_zobecneni_mereni/test.py
+++ 18_Algoritmizace/5_Lodicky_zobecneni_mereni/test.py
@@ -1,12 +1,14 @@
+import itertools
+
 perms = [78, 69, 106, 140]
+n = len(perms)
 
-i = 0
-j = 0
-
-output = []
-
-for i in range(0, len(perms)):
-    for j in range(0, 8):
-        output.append()
-
-print(output)
+for i in range(0, n):
+    for j in range(i+1, n):
+        perm = [None]*n
+        perm[0] = perms[i]
+        perm[n-1] = perms[j]
+        for p in itertools.permutations(perm[1:n]):
+            for t in range(1, n-1):
+                perm[t] = p[t-1]
+            print(perm)
